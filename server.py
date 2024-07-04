42500b53f9df56b2f448be38d044c256f6368272
Patrikc06 <168948961+Patrickby06@users.noreply.github.com>
2024-07-04 03:43:08 +0000

    Mundo guardado 2024-07-04

--- server.py
+++ server.py
@@ -8,7 +8,7 @@
 # Regiones de ngrok
 # Código          Lugar
 #-----------      ---------------------------
-# ap	          Asia / Pacífico (Singapore)asdasdad
+# ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
